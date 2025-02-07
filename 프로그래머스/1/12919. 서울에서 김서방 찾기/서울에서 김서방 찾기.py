3a16151ff721534084916ac2da9876c1fe9f7334
SYSY5959 <sysy59@hanyang.ac.kr>
2025-02-08 01:41:13 +0900

    Update 서울에서 김서방 찾기.py

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,7 +1,18 @@
+### 내 풀이
 def solution(seoul):
     n = 0
     for i in range(len(seoul)):
         if seoul[i] == "Kim":
             n = i
     answer = "김서방은 " + str(n) +"에 있다"
+    return answer
+
+
+### 더 간단한 풀이
+def solution(seoul):
+    
+    answer = ''
+    answer = "김서방은 {}에 있다".format(seoul.index('Kim'))
+    ## "~~~ {} ~~".format(a) : 문자열 안에 {}에 a 들어감
+    
     return answer
