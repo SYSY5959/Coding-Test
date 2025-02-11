e958459a5d8d1c7e80d4b99c223d74bb4a28077e
SYSY5959 <sysy59@hanyang.ac.kr>
2025-02-11 23:11:15 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.13 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -0,0 +1,30 @@
+def solution(n, arr1, arr2):
+    answer = []
+    a1 = []
+    a2 = []
+    
+    for num in arr1:
+        b = bin(num)[2:]
+        if len(b) < n:
+            b = "0"*(n - len(b)) + b
+        a1.append(b)
+        
+    for num2 in arr2:
+        b2 = bin(num2)[2:]
+        if len(b2) < n:
+            b2 = "0"*(n - len(b2)) + b2
+        a2.append(b2)
+        
+    for i in range(n):
+        s = ""
+        for j in range(n): # 하나의 2진수 문자열 안에서 
+            if a1[i][j] == "0" and a2[i][j] == "0":
+                s += "0"
+            elif a1[i][j] == "1" or a2[i][j] == "1":
+                s += "1"
+        s = s.replace("0", " ")
+        s = s.replace("1", "#")
+        #print(s)
+        answer.append(s)
+    
+    return answer
