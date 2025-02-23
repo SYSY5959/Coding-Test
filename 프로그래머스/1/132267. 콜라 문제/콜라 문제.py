d644bdd46f6651b772bbd263c7bffe0fac60c341
SYSY5959 <sysy59@hanyang.ac.kr>
2025-02-23 23:51:52 +0900

    Update 콜라 문제.py

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.py
@@ -1,8 +1,8 @@
 def solution(a, b, n):
     answer = 0
     while n >= a:
-        count = (n//a) * b
-        left = n%a
+        count = (n//a) * b # 받은 콜라 개수
+        left = n%a # 추가 콜라를 받지 못한 콜라 개수 (나머지)
         answer += count
-        n = count + left
+        n = count + left # 합친 걸로 또 콜라 받기
     return answer
