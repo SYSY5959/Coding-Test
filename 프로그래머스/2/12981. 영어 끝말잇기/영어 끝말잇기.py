2738abbde5b8096fb549357c25866d740cd97e44
SYSY5959 <sysy59@hanyang.ac.kr>
2025-02-26 16:38:56 +0900

    Update 영어 끝말잇기.py

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -8,3 +8,13 @@ def solution(n, words):
             return [i%n +1 , i//n +1]
     
     return [0,0]
+
+## 더 간단한 풀이
+def solution(n, words):
+    
+    for i in range(1, len(words)):
+        #글자가 맞지 않을 때
+        if words[i][0] != words[i-1][-1] or words[i] in words[:i]:
+            return [(i%n)+1, (i//n)+1]
+    
+    return [0,0]
