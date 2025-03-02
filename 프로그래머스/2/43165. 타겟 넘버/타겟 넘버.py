a444adb076cfbf52c22fa6777d1bdc031d8ec121
SYSY5959 <sysy59@hanyang.ac.kr>
2025-03-03 00:14:51 +0900

    Update 타겟 넘버.py

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -14,3 +14,24 @@ def solution(numbers, target):
     dfs(0, 0)
     return answer
 
+## 다른 풀이 : BFS로
+def solution(numbers, target):
+  leaves = [0]          # 모든 계산 결과를 리스트로 다 담기      
+  count = 0 
+
+  for num in numbers : 
+    temp = []
+	
+    # 자손 노드 
+    for leaf in leaves : # 앞 단계에서 계산한 리스트에 새로운 숫자 더하고 빼는 경우 다시 담기
+      temp.append(leaf + num)    # 더하는 경우 
+      temp.append(leaf - num)    # 빼는 경우 
+
+    leaves = temp 
+
+  # 모든 경우의 수 계산 후 target과 같은지 확인 
+  for leaf in leaves : 
+    if leaf == target : 
+      count += 1
+
+  return count
