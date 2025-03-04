b0150d4c7e1c75d94050b4c94652202d2e81f704
SYSY5959 <sysy59@hanyang.ac.kr>
2025-03-04 14:05:28 +0900

    [level 2] Title: 주식가격, Time: 71.34 ms, Memory: 19.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -0,0 +1,15 @@
+from collections import deque
+def solution(prices):
+    answer = []
+    queue = deque(prices)
+    
+    while queue:
+        a = queue.popleft() # 3 : 현재 주식 가격
+        s = 0
+        for q in queue: #[2,3] : 이후 주식 가격
+            s +=1
+            if q < a: # 주식 가격이 떨어질 때
+                break
+        answer.append(s)          
+        
+    return answer
