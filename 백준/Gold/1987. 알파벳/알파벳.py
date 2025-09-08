4b54b7aa34b0c23bc5bd1c68ceeb211eaddf8abc
SYSY5959 <sysy59@hanyang.ac.kr>
2025-09-08 17:07:47 +0900

    Update 알파벳.py

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -35,4 +35,38 @@ def bfs():
 R,C = map(int, input().split())
 arr = list(input() for _ in range(R))
 ans = bfs()
+
+print(ans)
+
+
+### DFS로 푸는 방법
+
+def dfs(ci,cj,cnt): 
+    # cnt: 현재 탐색하고 있는 딱 하나의 경로 길이 (중복되지 않은 알파벳의 수)
+    # ans: 지금까지 탐색했던 모든 경로들 중에서, 가장 길었던 값만 저장하는 기록용 변수!!!
+
+    global ans
+    ans = max(ans, cnt)
+
+    # 4방향, 범위내, 중복값이 아닌 경우
+    for di,dj in ((-1,0),(1,0),(0,1),(0,-1)):
+        ni,nj = ci+di,cj+dj
+        if 0<=ni<R and 0<=nj<C and v[ord(arr[ni][nj])]==0:
+            v[ord(arr[ni][nj])] = 1
+            dfs(ni,nj,cnt+1)
+
+            # 백트래킹 : 재귀호출이 끝나고 돌아오면, 방문 표시 해제
+            # -> 다른 경로 탐색할 때, 이 알파벳 사용 가능
+            # ex) A->B->C 탐색 후, A->D 탐색하기 위해, B,C 방문기록 지움!!!
+            v[ord(arr[ni][nj])] = 0
+
+
+R,C = map(int, input().split())
+arr = list(input() for _ in range(R))
+
+ans = 1
+v = [0]*128     # 아스키코드 128개
+
+v[ord(arr[0][0])] = 1   # 해당 값을 사용함. 방문표시
+dfs(0,0,1)
 print(ans)
