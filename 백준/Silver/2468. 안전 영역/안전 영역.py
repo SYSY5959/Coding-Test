16ddd9759e7fafd3e90bb4cda1744ee3c58e8b3b
SYSY5959 <sysy59@hanyang.ac.kr>
2025-09-22 23:47:03 +0900

    [Silver I] Title: 안전 영역, Time: 556 ms, Memory: 34984 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+def bfs(si,sj,h):
+    q = deque()
+    q.append((si,sj))
+    v[si][sj] = 1
+
+    while q:
+        ci,cj = q.popleft()
+        for di,dj in ((1,0),(-1,0),(0,1),(0,-1)):
+            ni,nj = ci+di,cj+dj
+            if 0<=ni<N and 0<=nj<N and v[ni][nj] == 0 and arr[ni][nj] > h:
+                q.append((ni,nj))
+                v[ni][nj] = 1
+
+
+## h 높이에 대해 잠기지 않는 영역 개수
+def solve(h):
+    cnt = 0 # 덩어리 개수
+    for i in range(N):
+        for j in range(N):
+            if v[i][j] == 0 and arr[i][j] > h:
+                bfs(i,j,h)
+                cnt +=1
+    return cnt
+
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+
+ans = 0
+for h in range(100):
+    v = [[0]*N for _ in range(N)]
+    ans = max(ans, solve(h))
+
+print(ans)
