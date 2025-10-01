39f29be163f71667cc6eca9b862bd277e1bb0c1e
SYSY5959 <sysy59@hanyang.ac.kr>
2025-10-01 21:32:54 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.16 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,16 @@
+
+def solution(num):
+    answer = 0
+    
+    while num != 1:
+        if answer >= 500:
+            answer = -1
+            break
+        if answer < 500 and num%2 == 0:
+            num = num//2
+            answer+=1
+        elif answer < 500 and num%2 == 1:
+            num = num*3 + 1
+            answer +=1
+    
+    return answer
