ed311dad6e4e22fc9a07485999401f1352c1f690
SYSY5959 <sysy59@hanyang.ac.kr>
2025-10-03 15:59:34 +0900

    [level 2] Title: 가장 큰 수, Time: 1322.60 ms, Memory: 26.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=python3) 
 
 ### 성능 요약
 
-메모리: 27.1 MB, 시간: 1387.16 ms
+메모리: 26.1 MB, 시간: 1322.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 01일 17:27:52
+2025년 10월 03일 15:59:31
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,12 +1,6 @@
 def solution(numbers):
     answer = ''
-    
-    # 숫자 문자열을 정렬하면 사전식으로 정렬됨 
-    # ['9', '5', '34', '30', '3'] 근데 여기서 30과 3의 자리를 바꾸고 싶으면
-    # sorted함수에서 key로 커스텀. "3030303030.." vs "3333333.." 비교하게 됨
 
-    numbers = sorted(map(str, numbers), key=lambda x: x*3, reverse=True)
-    #answer = "".join(numbers) -> 이렇게 하면 [0,0] 일때 '00'이 나옴!!!
-    answer = str(int("".join(numbers)))
-    
+    lst = sorted(map(str, numbers), key=lambda x:x*3 , reverse = True)
+    answer = str(int("".join(lst)))
     return answer
