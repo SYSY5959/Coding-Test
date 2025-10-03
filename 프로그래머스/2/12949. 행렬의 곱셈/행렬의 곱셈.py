224852ff345f769079871ca8143e57f9da60cbc9
SYSY5959 <sysy59@hanyang.ac.kr>
2025-10-03 12:45:43 +0900

    [level 2] Title: 행렬의 곱셈, Time: 96.90 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -0,0 +1,11 @@
+def solution(arr1, arr2):
+    n = len(arr1)
+    m = len(arr2[0])
+    l = len(arr1[0])
+    answer = [[0]*m for _ in range(n)]
+    
+    for i in range(n):
+        for j in range(m):
+            for k in range(l):
+                answer[i][j] += arr1[i][k]*arr2[k][j]
+    return answer
