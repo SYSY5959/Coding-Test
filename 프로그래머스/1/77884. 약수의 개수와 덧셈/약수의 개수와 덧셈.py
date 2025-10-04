8cbd2214874ee107753ec3855c6be37a44be0bf2
SYSY5959 <sysy59@hanyang.ac.kr>
2025-10-05 01:43:24 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 21.71 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -0,0 +1,15 @@
+def func(n):
+    cnt = 0
+    for i in range(1,n+1):
+        if (n%i ==0):
+            cnt +=1
+    return cnt
+
+def solution(left, right):
+    answer = 0
+    for n in range(left,right+1):
+        if func(n) % 2 == 0:
+            answer += n
+        else:
+            answer -= n
+    return answer
