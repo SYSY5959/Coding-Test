8b1254a39c39ba7034b7bebf68617f826f75a116
SYSY5959 <sysy59@hanyang.ac.kr>
2025-10-07 17:22:59 +0900

    [level 1] Title: 기사단원의 무기, Time: 1342.82 ms, Memory: 9.38 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,18 @@
+def cal(n):
+    cnt = 0
+    for i in range(1,int(n**0.5)+1):
+        if n % i == 0:
+            cnt+=1
+            if i != n//i:
+                cnt+=1
+    return cnt
+        
+def solution(number, limit, power):
+    answer = 0
+    for n in range(1, number+1):
+        cnt = cal(n)
+        if cnt > limit:
+            answer += power
+        else:
+            answer += cnt
+    return answer
